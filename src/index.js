76ac6542e3372d19bedb730e2bd9a2f3512564f0
doubleface <christophe@cozycloud.cc>
2018-04-11 09:45:23 +0200

    feat: login

--- src/index.js
+++ src/index.js
@@ -1,49 +1,35 @@
 const {
   BaseKonnector,
   requestFactory,
-  saveFiles,
-  addData
+  log
 } = require('cozy-konnector-libs')
-const request = requestFactory({ cheerio: true })
+const request = requestFactory({
+  // debug: true,
+  cheerio: true,
+  jar: true
+})
 
-const baseUrl = 'http://books.toscrape.com'
+const baseUrl = 'https://cfspart.impots.gouv.fr'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
-function start(fields) {
-  // The BaseKonnector instance expects a Promise as return of the function
-  return request(`${baseUrl}/index.html`).then($ => {
-    // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
-    // here I do an Array.from to convert the cheerio fake array to a real js array.
-    const entries = Array.from($('article')).map(article =>
-      parseArticle($, article)
-    )
-    return addData(entries, 'io.cozy.books').then(() =>
-      saveFiles(entries, fields)
-    )
+async function start (fields) {
+  await login(fields)
+}
+
+async function login (fields) {
+  log('info', 'Logging in')
+  await request(`${baseUrl}/LoginMDP`)
+  await request({
+    method: 'POST',
+    uri: `${baseUrl}/LoginMDP?op=c&url=`,
+    form: {
+      url: '',
+      LMDP_Spi: fields.login,
+      LMDP_Password: fields.password,
+      LMDP_Spi_tmp: fields.login,
+      LMDP_Password_tmp: fields.password
+    }
   })
-}
-
-// The goal of this function is to parse a html page wrapped by a cheerio instance // and return an array of js objects which will be saved to the cozy by addData (https://github.com/cozy/cozy-konnector-libs/blob/master/docs/api.md#module_addData)
-// and saveFiles (https://github.com/cozy/cozy-konnector-libs/blob/master/docs/api.md#savefiles)
-function parseArticle($, article) {
-  const $article = $(article)
-  const title = $article.find('h3 a').attr('title')
-  return {
-    title,
-    price: normalizePrice($article.find('.price_color').text()),
-    url: `${baseUrl}/${$article.find('h3 a').attr('href')}`,
-    // when it finds a fileurl attribute, saveFiles will save this file to the cozy with a filename
-    // name
-    fileurl: `${baseUrl}/${$article.find('img').attr('src')}`,
-    filename: `${title}.jpg`
-  }
-}
-
-// convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.trim().replace('£', ''))
+  // TODO check LOGIN_FAILED
 }
