f969d545960c1cf4776649b26cc3555d647cc80c
doubleface <christophe@cozycloud.cc>
2018-04-12 15:50:05 +0200

    feat: file saving implementation

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ const {
   BaseKonnector,
   requestFactory,
   log,
-  scrape
+  scrape,
+  saveFiles,
+  errors
 } = require('cozy-konnector-libs')
 const request = requestFactory({
   // debug: true,
   cheerio: true,
-  jar: true
+  jar: true,
+  json: false
 })
+const querystring = require('querystring')
 
 const baseUrl = 'https://cfspart.impots.gouv.fr'
 
@@ -16,7 +20,36 @@ module.exports = new BaseKonnector(start)
 
 async function start (fields) {
   await login(fields)
-  await fetch()
+  const documents = await fetch()
+  await saveFiles(documents, fields)
+}
+
+async function login (fields) {
+  log('info', 'Logging in')
+  let $
+  try {
+    $ = await request({
+      method: 'POST',
+      uri: `${baseUrl}/LoginMDP?op=c&url=`,
+      form: {
+        url: '',
+        LMDP_Spi: fields.login,
+        LMDP_Password: fields.password,
+        LMDP_Spi_tmp: fields.login,
+        LMDP_Password_tmp: fields.password
+      }
+    })
+  } catch (err) {
+    log('error', 'Website failed while trying to login')
+    log('error', err.message)
+    throw new Error(errors.VENDOR_DOWN)
+  }
+
+  const erreurs = $('.erreur:not(.pasvisible)')
+  if (erreurs.length) {
+    log('error', erreurs.eq(0).text().trim())
+    throw new Error(errors.LOGIN_FAILED)
+  }
 }
 
 async function fetch () {
@@ -25,6 +58,7 @@ async function fetch () {
 
   // get the "Mes documents" link
   const documentsLink = $('img[name=doc]').closest('a').attr('href')
+  const urlPrefix = documentsLink.split('/')[1] // gets "cesu-XX" from the url
   $ = await request(`${baseUrl}${documentsLink}`)
 
   const $form = $('form[name=documentsForm]')
@@ -43,28 +77,41 @@ async function fetch () {
     }
   })
 
-  const docs = scrape($, {
-    'fileurl': {
-      attr: 'onclick'
+  const documents = scrape($, {
+    fileurl: {
+      attr: 'onclick',
+      parse: onclick => {
+        const viewerUrl = onclick.match(/\((.*)\)/)[1].split(',')[0].slice(1, -1)
+        return `${baseUrl}/${urlPrefix}/${viewerUrl}`
+      }
+    },
+    name: {
+      fn: link => {
+        return $(link).closest('tr').text()
+      }
     }
   }, '.cssLienTable')
 
-  console.log(docs, 'docs')
-}
+  log('info', `Found ${documents.length} documents to download`)
 
-async function login (fields) {
-  log('info', 'Logging in')
-  await request(`${baseUrl}/LoginMDP`)
-  await request({
-    method: 'POST',
-    uri: `${baseUrl}/LoginMDP?op=c&url=`,
-    form: {
-      url: '',
-      LMDP_Spi: fields.login,
-      LMDP_Password: fields.password,
-      LMDP_Spi_tmp: fields.login,
-      LMDP_Password_tmp: fields.password
+  const result = []
+  for (let doc of documents) {
+    log('debug', `Fetching doc url for ${doc.name}`)
+    const $ = await request(doc.fileurl)
+    result.push({
+      fileurl: `${baseUrl}${$('iframe').attr('src')}`,
+      name: doc.name
+    })
+  }
+
+  return result.map(doc => {
+    if (doc.fileurl.match(/ConsultAR/)) {
+      // we have an "accusé de reception" without a file name
+      log('info', 'Old accuse de reception without filename')
+      const {typeForm, annee, numeroAdonis} = querystring.parse(doc.fileurl)
+      doc.filename = `IR-${typeForm}--${annee}-${numeroAdonis}.pdf`
+      log('info', 'Changed filename to ' + doc.filename)
     }
+    return doc
   })
-  // TODO check LOGIN_FAILED
 }
