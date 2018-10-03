947a5f595fc3e4fa7823bfdca1c5c2ff1dbc9d5b
LucsT <lucas@cozycloud.cc>
2018-10-03 16:34:22 +0200

    feat: Make bills from the detailsaccount tab, refund not implemented

--- src/index.js
+++ src/index.js
@@ -8,14 +8,17 @@ const {
   log,
   scrape,
   saveFiles,
+  saveBills,
   errors
 } = require('cozy-konnector-libs')
 const request = requestFactory({
-  // debug: true,
+  //debug: true,
   cheerio: true,
   jar: true,
   json: false
 })
+const moment = require('moment')
+
 const normalizeFileNames = require('./fileNamer')
 
 const baseUrl = 'https://cfspart.impots.gouv.fr'
@@ -24,8 +27,11 @@ module.exports = new BaseKonnector(start)
 
 async function start(fields) {
   await login(fields)
-  const documents = await fetch()
+  const [documents, bills] = await fetch()
   await saveFiles(documents, fields)
+  await saveBills(bills, fields, {
+    identifiers: ['impots']
+  })
 }
 
 async function login(fields) {
@@ -65,31 +71,44 @@ async function login(fields) {
 }
 
 async function fetch() {
-  let { $, urlPrefix } = await fetchMyDocumentsPage()
+  /* Mandatory: Fetch details before documents, because pdf access is selective.
+     Hopefully, 'details' pdfs are include in 'all documents' pdfs.
+  */
+  let { urlPrefix, token } = await fetchMenu()
 
+  let $ = await getMyDetailAccountPage(urlPrefix, token)
+  const bills = await parseBills($, urlPrefix)
+
+  $ = await getMyDocumentsPage(urlPrefix, token)
   const documents = parseMyDocuments($, urlPrefix)
-  const result = await fetchFilesUrls(documents)
-  return normalizeFileNames(result)
+
+  const documentsFetched = await prefetchUrls(documents)
+  const billsFetched = await prefetchUrls(bills)
+
+  return [
+    normalizeFileNames(documentsFetched),
+    normalizeFileNames(billsFetched)
+  ]
 }
 
-async function fetchMyDocumentsPage() {
-  // default Mes Documents page
-  log('info', 'Fetching the list of documents')
+async function fetchMenu() {
   let $ = await request(`${baseUrl}/acces-usager/cfs`)
   const documentsLink = $('img[name=doc]')
     .closest('a')
     .attr('href')
-  const urlPrefix = documentsLink.split('/')[1] // gets "cesu-XX" from the url
+  const urlPrefix = documentsLink.split('/')[1] // gets "cesu-XX" or "cfsu-XX" from the url
   $ = await request(`${baseUrl}${documentsLink}`)
-
-  // full Mes Documents page with all the documents
   const $form = $('form[name=documentsForm]')
-  const formUrl = $form.attr('action')
   const token = $form.find('input[name=CSRFTOKEN]').val()
+  return { urlPrefix, token }
+}
 
-  $ = await request({
+async function getMyDocumentsPage(urlPrefix, token) {
+  log('info', 'Fetching the documents page')
+
+  const $ = await request({
     method: 'POST',
-    uri: `${baseUrl}${formUrl}`,
+    uri: `${baseUrl}/${urlPrefix}/documents.html`,
     form: {
       annee: 'all',
       CSRFTOKEN: token,
@@ -98,8 +117,25 @@ async function fetchMyDocumentsPage() {
       typeImpot: 'toutImpot'
     }
   })
+  return $
+}
 
-  return { $, urlPrefix }
+async function getMyDetailAccountPage(urlPrefix, token) {
+  log('info', 'Fetching the MyDetailAccount page')
+  const $ = await request({
+    method: 'POST',
+    uri: `${baseUrl}/${urlPrefix}/compteRedirection.html`,
+    form: {
+      annee: 'all',
+      CSRFTOKEN: token,
+      method: 'redirection',
+      date: 'gardeDate',
+      typeImpot: 'toutImpot',
+      tresorerieCodee: 'toutesTresoreries',
+      compte: 'compteDetaille'
+    }
+  })
+  return $
 }
 
 function parseMyDocuments($, urlPrefix) {
@@ -133,15 +169,127 @@ function parseMyDocuments($, urlPrefix) {
   return documents
 }
 
-async function fetchFilesUrls(documents) {
+async function parseBills($, urlPrefix) {
+  /* This big function scrape the tab with multiple column size and
+     no specific characteristics to stick
+  */
+  log('info', 'Parsing bills from detailAccount page')
+  let masterBillLink
+  let bills = []
+  const lines = Array.from($('td[class=cssFondTable]').find('table tr'))
+  lines.forEach((line, index) => {
+    const $line = $(line)
+    if (
+      // Throw first line as title
+      index === 0
+    ) {
+      log('debug', 'Throw line 0 as title')
+    } else if (
+      // If line is a master line(2 column), store link
+      $line.find('td').length === 2 &&
+      $line
+        .find('td')
+        .eq(1)
+        .attr('colspan') == 5
+    ) {
+      log('debug', `Master line detected`)
+      masterBillLink = extractLinkInMasterLine($line)
+    } else if (
+      // If line is a same year master line(1 column), store link
+      $line.find('td').length === 1 &&
+      $line.find('td').attr('colspan') == 5
+    ) {
+      log('debug', `Master same year line detected`)
+      masterBillLink = extractLinkInMasterLine($line)
+    } else if (
+      // If line is a payment (3 column), make a bill
+      $line.find('td').length === 3 &&
+      $line
+        .find('td')
+        .eq(2)
+        .text() != '\xa0' // Unbreakable-space as empty cell
+    ) {
+      log('debug', 'Payment line with 3 column detected')
+      let bill = scrapeLine($line, masterBillLink, urlPrefix, 3)
+      bills.push(bill)
+    } else if (
+      // If line is a payment(5 column), make a bill
+      $line.find('td').length === 5 &&
+      $line
+        .find('td')
+        .eq(3)
+        .text() != '\xa0' // Unbreakable-space as empty cell
+    ) {
+      log('debug', 'Payment line with 5 column detected')
+      let bill = scrapeLine($line, masterBillLink, urlPrefix, 5)
+      bills.push(bill)
+    } else if (
+      // If line is a refund
+      $line.find('td').length === 1 &&
+      $line.find('td').attr('colspan') == 4
+    ) {
+      log('debug', 'Refund line')
+      // To be implemented
+    }
+  })
+  return bills
+}
+
+function extractLinkInMasterLine($line) {
+  let link = $line.find('a').attr('onclick')
+  if (link != undefined) {
+    link = link.split(`'`)[1]
+  }
+  return link
+}
+
+function scrapeLine($line, masterBillLink, urlPrefix, model) {
+  let amountCol, dateCol
+  if (model == 3) {
+    amountCol = 2
+    dateCol = 0
+  } else if (model == 5) {
+    amountCol = 3
+    dateCol = 1
+  }
+  const amount = $line
+    .find('td')
+    .eq(amountCol)
+    .text()
+  const date = $line
+    .find('td')
+    .eq(dateCol)
+    .text()
+    .match(/\d{2}\/\d{2}\/\d{4}/)[0]
+  return {
+    vendor: 'impots',
+    amount: parseFloat(amount.match(/\d+/g).join('')),
+    currency: 'EUR',
+    date: moment(
+      date.match(/\d{1,2}\/\d{1,2}\/\d{4}/)[0],
+      'DD-MM-YYYY'
+    ).toDate(),
+    fileurl: masterBillLink
+      ? `${baseUrl}/${urlPrefix}/${masterBillLink}`
+      : undefined
+  }
+}
+
+async function prefetchUrls(documents) {
   const result = []
   for (let doc of documents) {
-    log('debug', `Fetching doc url for ${doc.name}`)
-    const $ = await request(doc.fileurl)
-    result.push({
-      fileurl: `${baseUrl}${$('iframe').attr('src')}`,
-      name: doc.name
-    })
+    if (doc.fileurl == undefined) {
+      log('debug', 'No url provide, delete attribut')
+      delete doc.fileurl
+      result.push(doc)
+    } else {
+      log('debug', `Prefetching url for ${doc.fileurl}`)
+      const $ = await request(doc.fileurl)
+      result.push({
+        ...doc,
+        fileurl: `${baseUrl}${$('iframe').attr('src')}`
+      })
+    }
   }
   return result
 }
